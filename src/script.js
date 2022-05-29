84e1c639330f237c7d45c694a84ec27668b39a98
mwynyard <101786771+mwynyard@users.noreply.github.com>
2022-05-29 22:37:56 +1000

    removed API, replaced

--- src/script.js
+++ src/script.js
@@ -1,72 +1,39 @@
 const submit = document.querySelector(".submit-button")
 const resultContent = document.querySelector(".result")
-
-const showContent = (event) => {
-    resultContent.style.display = "block"
-    event.preventDefault();
-    console.log("FORM SUBMITTED")
-}
-
-// submit.addEventListener('click', showContent)
-
-// const options = {method: 'GET', headers: {Accept: 'application/json'}};
-
-// fetch('https://api.debounce.io/v1/?api=627238f52d347&email=contact%40talentrecruit.com', options)
-//   .then(response => response.json())
-//   .then(response => console.log(response))
-//   .catch(err => console.error(err));
-
-  //API KEY 627238f52d347
-
-let emailInput = document.querySelector('#email-input')
-let emailVal = emailInput.value
+const emailInput = document.querySelector('#email-input')
+const emailVal = emailInput.value
 
 
 var formEl = document.getElementById('form');
+var token = config.MY_API_TOKEN
 
 formEl.addEventListener('submit', function(event) {
-  // 1. Setup the request
-  // ================================
-  // 1.1 Headers
+
   var headers = new Headers();
-  // Tell the server we want JSON back
+
   headers.set('Accept', 'application/json');
 
-  // Show Results
   resultContent.style.display = "block";
   const emailTest = "milan.wynyard%40gmail.com"
 
-  // 1.2 Form Data
-  // We need to properly format the submitted fields.
-  // Here we will use the same format the browser submits POST forms.
-  // You could use a different format, depending on your server, such
-  // as JSON or XML.
   var formData = new FormData();
   for (var i = 0; i < formEl.length; ++i) {
     formData.append(formEl[i].name, formEl[i].value);
   }
-  
-  // This is for the purpose of this demo using jsFiddle AJAX Request endpoint
   formData.append('json', JSON.stringify({example: 'return value'}));
 
-  // 2. Make the request
-  // ================================
-  var url = `https://api.debounce.io/v1/?api=627238f52d347&email=${emailInput.value}`;
+  var url = `https://api.debounce.io/v1/?api=${token}&email=${emailInput.value}`;
   var fetchOptions = {
     method: 'POST', headers,
     body: formData
   };
   
   var responsePromise = fetch(url, fetchOptions)
-  
-  // 3. Use the response
-  // ================================
+
   responsePromise
-  	// 3.1 Convert the response into JSON-JS object.
     .then(function(response) {
       return response.json();
     })
-    // 3.2 Do something with the JSON data
     .then(function(jsonData) {
     	console.log(jsonData.debounce);
       document.getElementById('results').innerText =
@@ -81,5 +48,3 @@ formEl.addEventListener('submit', function(event) {
 
   event.preventDefault();
 });
-
-//info@urbanedge.com.au
